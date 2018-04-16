92f60d30661b06c1f8c0f941fc4545b11a29e19f
Carter Chavez <Carter.chavez17@gmail.com>
2018-04-15 22:05:04 -0700

    fix style

--- src/components/WeatherHome.jsx
+++ src/components/WeatherHome.jsx
@@ -7,18 +7,18 @@ import DaysList from './DaysList';
 
 
 class WeatherHome extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			location: {lat: null, long: null},
+    this.state = {
+      location: {lat: null, long: null},
       locationName: '',
       days: []
-		}
+    }
 
-		this.searchGeolocation = this.searchGeolocation.bind(this);
+    this.searchGeolocation = this.searchGeolocation.bind(this);
     this.createDays = this.createDays.bind(this);
-	}
+  }
 
   createDays(timeInputs) {
     let dailyData = timeInputs.data.daily.data;
@@ -27,45 +27,42 @@ class WeatherHome extends Component {
     })
   }
 
-	searchGeolocation(address) {
-		let geocoder = new google.maps.Geocoder();
-		geocoder.geocode({address: address}, (res, status) => {
-			if ( status === 'OK' ) {
-
+  searchGeolocation(address) {
+    let geocoder = new google.maps.Geocoder();
+    geocoder.geocode({address: address}, (res, status) => {
+      if ( status === 'OK' ) {
         this.setState({
           locationName:res[0].formatted_address
         });
 
-				let lat = res[0].geometry.location.lat();
-				let lng = res[0].geometry.location.lng();
-				axios.get('http://localhost:4568/search', {params: {lat:lat, lng:lng }})
-					.then((weather) => {
+        let lat = res[0].geometry.location.lat();
+        let lng = res[0].geometry.location.lng();
+        axios.get('http://localhost:4568/search', {params: {lat:lat, lng:lng }})
+          .then((weather) => {
             this.createDays(weather);
             console.log(this.state)
-					})
-					.catch((error) => {
-						console.log('Error: ', error)
-					})
-			} else {
-				console.log('Error: ', status);
-			}
-		})
-	}
+          })
+          .catch((error) => {
+            console.log('Error: ', error);
+          })
+      } else {
+        console.log('Error: ', status);
+      }
+    })
+  }
 
-	render() {
-		return (
-	    <div>
-	    	<Search searchGeolocation={this.searchGeolocation} />
-
+  render() {
+    return (
+      <div>
+        <Search searchGeolocation={this.searchGeolocation} />
         <DaysList 
           locationName={this.state.locationName} 
           days={this.state.days} 
         />
-
-	      <Link to="/tests">Go To Test Page</Link>
-	    </div>
-	  )
-	}
+        <Link to="/tests">Go To Test Page</Link>
+      </div>
+    )
+  }
 }
 
 
