b6708e7b1083dc61e5434e4fe86728b2574dee28
Carter Chavez <Carter.chavez17@gmail.com>
2018-04-18 22:01:09 -0700

    test server

--- server/index.js
+++ server/index.js
@@ -17,22 +17,22 @@ app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.all('*',function(req,res,next) {
-  if (!req.get('Origin')) {
-    return next();
-  } 
+// app.all('*',function(req,res,next) {
+//   if (!req.get('Origin')) {
+//     return next();
+//   } 
 
 
-  res.set('Access-Control-Allow-Origin','http://localhost:8080');
-  res.set('Access-Control-Allow-Methods','GET,POST');
-  res.set('Access-Control-Allow-Headers','X-Requested-With,Content-Type');
+//   res.set('Access-Control-Allow-Origin','http://localhost:8080');
+//   res.set('Access-Control-Allow-Methods','GET,POST');
+//   res.set('Access-Control-Allow-Headers','X-Requested-With,Content-Type');
 
-  if ('OPTIONS' == req.method) {
-    return res.send(200);
-  }
+//   if ('OPTIONS' == req.method) {
+//     return res.send(200);
+//   }
 
-  next();
-});
+//   next();
+// });
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
